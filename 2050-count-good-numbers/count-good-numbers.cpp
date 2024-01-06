bac19fc09abfd2c67b1e2b067a5caf6d1389f4ba
Rishabh Shukla <110485006+rishabh-i13@users.noreply.github.com>
2024-01-06 16:29:12 +0530

    Time: 3 ms (26.40%) | Memory: 6.5 MB (7.35%) - LeetSync

---
+++ 2050-count-good-numbers/count-good-numbers.cpp
@@ -0,0 +1,31 @@
+class Solution {
+public:
+    long long M = 1e9 + 7;
+    long long power(long long N, long long R)
+
+    {
+        if (R == 0)
+            return 1;
+        if (R == 1)
+            return (N % 1000000007);
+        long long ans = 1;
+        long long know = power(N, R / 2) % 1000000007;
+        if (R % 2)
+            ans = (ans * N) % 1000000007;
+        return (ans * ((know * know) % 1000000007)) % 1000000007;
+    }
+    int countGoodNumbers(long long n) {
+        long long ans = 0;
+        if (n % 2 == 0) {
+            long long int even = power(5, n / 2) % M;
+            long long int prime = power(4, n / 2) % M;
+            ans = (even * prime) % M;
+            return ans;
+        } else {
+            long long int even = power(5, (n + 1) / 2) % M;
+            long long int prime = power(4, n / 2) % M;
+            ans = (even * prime) % M;
+            return ans;
+        }
+    }
+};
