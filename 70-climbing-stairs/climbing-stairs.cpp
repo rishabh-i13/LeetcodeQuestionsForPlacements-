19b7f39d8e1e3f1e79859299fa1d8a423585133f
Rishabh Shukla <110485006+rishabh-i13@users.noreply.github.com>
2024-03-16 20:34:37 +0530

    Time: 3 ms (17.76%) | Memory: 7.5 MB (11.97%) - LeetSync

--- 70-climbing-stairs/climbing-stairs.cpp
+++ 70-climbing-stairs/climbing-stairs.cpp
@@ -1,14 +1,14 @@
+
 class Solution {
 public:
+    int f(int n, vector<int> &dp){
+        if(n==1||n==0) return 1;
+        if(dp[n] != -1) return dp[n];
+        return dp[n] = f(n-1, dp)+f(n-2, dp);
+    }
     int climbStairs(int n) {
-        vector<int> dp(n + 1, -1);
-
-        dp[0] = 1;
-        dp[1] = 1;
-
-        for (int i = 2; i <= n; i++) {
-            dp[i] = dp[i - 1] + dp[i - 2];
-        }
-        return dp[n];
+        vector<int> dp(n+1, -1);
+        return f(n, dp);
+        
     }
 };
