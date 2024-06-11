2a4dd34dbb48c8faeb88b591282b518de24acae4
Rishabh Shukla <110485006+rishabh-i13@users.noreply.github.com>
2024-06-11 17:48:35 +0530

    Time: 0 ms (100.00%) | Memory: 9.3 MB (49.22%) - LeetSync

--- 213-house-robber-ii/house-robber-ii.cpp
+++ 213-house-robber-ii/house-robber-ii.cpp
@@ -1,29 +1,28 @@
 class Solution {
 public:
-    //1st and nth can not be taken at once as 
-    //this is circular , they are neighbour
-    
-    //first calculate from 1st house to n-1th house
-      //then calculate 2nd house to nth house
-    int working(vector<int> &nums,int index,int n,int t[]){
-        if(index>=n) return 0;
-        if(t[index]!=-1) return t[index];
-
-        int steal=nums[index]+working(nums,index+2,n,t);
-
-        int skip=working(nums,index+1,n,t);
-        return t[index]=max(steal,skip);
-    }
-
     int rob(vector<int>& nums) {
         int n=nums.size();
-        if (n==1) return nums[0];
-        int t1[100];
-        int t2[100];
-        memset(t1,-1,sizeof(t1));
-        memset(t2,-1,sizeof(t2));
-        // now return the max of (1st to n-1 house)
-        // and (2nd to nth house)
-        return max(working(nums,0,n-1,t1),working(nums,1,n,t2));
+        if(n==1) return nums[0];
+        if(n==2) return max(nums[0],nums[1]);
+        vector<int> dp(n+1);
+        dp[0]=0;
+
+        for(int i=1;i<=n-1;i++){
+            int steal=nums[i-1]+((i-2)>0?dp[i-2]:0);
+            int skip=dp[i-1];
+            dp[i]=max(steal,skip);
+        }
+        int case1=dp[n-1];
+        dp.clear();
+        dp[0]=0;
+        dp[1]=0;
+        for(int i=2;i<=n;i++){
+            int steal=nums[i-1]+((i-2)>=0 ? dp[i-2] : 0);
+            int skip=dp[i-1];
+            dp[i]=max(steal,skip);
+        }
+        int case2=dp[n];
+
+        return max(case1,case2);
     }
 };
