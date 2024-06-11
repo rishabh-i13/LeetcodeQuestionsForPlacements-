1d1d6f4b8fad9610e15b1092d4d4edc25b0e9a93
Rishabh Shukla <110485006+rishabh-i13@users.noreply.github.com>
2024-06-11 17:19:40 +0530

    Time: 4 ms (33.15%) | Memory: 9.1 MB (85.04%) - LeetSync

--- 213-house-robber-ii/house-robber-ii.cpp
+++ 213-house-robber-ii/house-robber-ii.cpp
@@ -1,31 +1,29 @@
 class Solution {
 public:
-    int t1[100];
-    int t2[100];
-    int work1(vector<int> &nums,int index,int n){
+    //1st and nth can not be taken at once as 
+    //this is circular , they are neighbour
+    
+    //first calculate from 1st house to n-1th house
+      //then calculate 2nd house to nth house
+    int working(vector<int> &nums,int index,int n,int t[]){
         if(index>=n) return 0;
-        if(t1[index]!=-1) return t1[index];
+        if(t[index]!=-1) return t[index];
 
-        int steal=nums[index]+work1(nums,index+2,n);
+        int steal=nums[index]+working(nums,index+2,n,t);
 
-        int skip=work1(nums,index+1,n);
-        return t1[index]=max(steal,skip);
+        int skip=working(nums,index+1,n,t);
+        return t[index]=max(steal,skip);
     }
-    int work2(vector<int> &nums,int index,int n){
-        if(index>=n) return 0;
-        if(t2[index]!=-1) return t2[index];
 
-        int steal=nums[index]+work2(nums,index+2,n);
-
-        int skip=work2(nums,index+1,n);
-        return t2[index]=max(steal,skip);
-    }
     int rob(vector<int>& nums) {
         int n=nums.size();
         if (n==1) return nums[0];
+        int t1[100];
+        int t2[100];
         memset(t1,-1,sizeof(t1));
         memset(t2,-1,sizeof(t2));
-
-        return max(work1(nums,0,n-1),work2(nums,1,n));
+        // now return the max of (1st to n-1 house)
+        // and (2nd to nth house)
+        return max(working(nums,0,n-1,t1),working(nums,1,n,t2));
     }
 };
