ee6b1c0418b24e2346f454425fda4bd281f09fe1
Rishabh Shukla <110485006+rishabh-i13@users.noreply.github.com>
2024-07-13 22:06:38 +0530

    Time: 234 ms (48.85%) | Memory: 210.7 MB (62.59%) - LeetSync

--- 2846-robot-collisions/robot-collisions.cpp
+++ 2846-robot-collisions/robot-collisions.cpp
@@ -1,54 +1,57 @@
 class Solution {
 public:
+    static bool compareBySecond(const pair<int, int>& a, const pair<int, int>& b) {
+        return a.second < b.second;
+    }
     vector<int> survivedRobotsHealths(vector<int>& positions, vector<int>& healths, string directions) {
+        vector<pair<int, int>> pos_index;
         int n = positions.size();
-        vector<vector<int>> robots;
-
-        for (int i = 0; i < n; ++i) {
-            robots.push_back({positions[i], healths[i], directions[i], i});
+        
+        for (int i = 0; i < n; i++) {
+            pos_index.push_back({positions[i], i});
         }
-
-        sort(robots.begin(), robots.end());
-
-        vector<vector<int>> stack;
-
-        for (auto& robot : robots) {
-            if (robot[2] == 'R' || stack.empty() || stack.back()[2] == 'L') {
-                stack.push_back(robot);
-                continue;
-            }
-
-            if (robot[2] == 'L') {
-                bool add = true;
-                while (!stack.empty() && stack.back()[2] == 'R' && add) {
-                    int last_health = stack.back()[1];
-                    if (robot[1] > last_health) {
-                        stack.pop_back();
-                        robot[1] -= 1;
-                    } else if (robot[1] < last_health) {
-                        stack.back()[1] -= 1;
-                        add = false;
+        
+        sort(begin(pos_index), end(pos_index));
+        
+        stack<pair<int, int>> st;
+        for (int i = 0; i < n; i++) {
+            int ind = pos_index[i].second;
+            if (st.empty() || directions[ind] == directions[st.top().second] || (directions[st.top().second]=='L' and directions[ind]=='R')) {
+                st.push(pos_index[i]);
+            } else {
+                while (!st.empty() && directions[ind] != directions[st.top().second]) {
+                    if (healths[ind] == healths[st.top().second]) {
+                        st.pop();
+                        healths[ind] = 0;
+                        break;
+                    } else if (healths[ind] < healths[st.top().second]) {
+                        healths[st.top().second]--;
+                        healths[ind] = 0;
+                        break;
                     } else {
-                        stack.pop_back();
-                        add = false;
+                        healths[ind]--;
+                        st.pop();
                     }
                 }
-
-                if (add) {
-                    stack.push_back(robot);
+                if (healths[ind] > 0) {
+                    st.push(pos_index[i]);
                 }
             }
         }
-
-        vector<int> result;
-        sort(stack.begin(), stack.end(), [](vector<int>& a, vector<int>& b) {
-            return a[3] < b[3];
-        });
-
-        for (auto& robot : stack) {
-            result.push_back(robot[1]);
+        
+        vector<pair<int, int>> v;
+        while (!st.empty()) {
+            v.push_back(st.top());
+            st.pop();
         }
-
-        return result;
+        
+        sort(v.begin(), v.end(), compareBySecond);
+        
+        vector<int> ans;
+        for (const auto& p : v) {
+            ans.push_back(healths[p.second]);
+        }
+        
+        return ans;
     }
 };
