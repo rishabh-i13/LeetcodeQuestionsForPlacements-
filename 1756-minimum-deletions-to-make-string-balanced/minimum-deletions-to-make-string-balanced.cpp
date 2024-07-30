bea5fe1da54850d1ce10cc81afab093622057d89
Rishabh Shukla <110485006+rishabh-i13@users.noreply.github.com>
2024-07-30 18:12:51 +0530

    Time: 93 ms (55.50%) | Memory: 24.5 MB (56.62%) - LeetSync

--- 1756-minimum-deletions-to-make-string-balanced/minimum-deletions-to-make-string-balanced.cpp
+++ 1756-minimum-deletions-to-make-string-balanced/minimum-deletions-to-make-string-balanced.cpp
@@ -1,19 +1,16 @@
 class Solution {
 public:
     int minimumDeletions(string s) {
-        int cnta = 0, cntb = 0, ans = 1e5;
-        for(auto i : s) cnta += i == 'a';
-        
-        // Delete all the a's that could be possible ans 
-        ans = cnta;
-
-        for(int i = 0 ; i<s.length() ; i++){
-            cntb += s[i] == 'b';
-            cnta -= s[i] == 'a';
-
-            ans = min(ans, cntb+cnta);
-        } 
-
-        return ans;
+        int n=s.size();
+        int count=0;
+        stack<char> st;
+         for(auto i : s){
+            if(!st.empty() and (i=='a' && st.top()=='b')){
+                count++;
+                st.pop();
+            }
+            else st.push(i);
+        }
+        return count;
     }
 };
