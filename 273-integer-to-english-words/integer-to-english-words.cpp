4afb32a27cdb6695d0755400385ffae663fd9319
Rishabh Shukla <110485006+rishabh-i13@users.noreply.github.com>
2024-08-07 13:38:22 +0530

    Time: 7 ms (19.94%) | Memory: 12.4 MB (19.59%) - LeetSync

--- 273-integer-to-english-words/integer-to-english-words.cpp
+++ 273-integer-to-english-words/integer-to-english-words.cpp
@@ -2,7 +2,7 @@ class Solution {
 public:
           unordered_map<int, string> below10 = { {0, ""}, {1, "One"}, {2, "Two"}, {3, "Three"}, {4, "Four"}, {5, "Five"}, {6, "Six"}, {7, "Seven"}, {8, "Eight"}, {9, "Nine"} };
     unordered_map<int, string> below20 = { {10, "Ten"}, {11, "Eleven"}, {12, "Twelve"}, {13, "Thirteen"}, {14, "Fourteen"}, {15, "Fifteen"}, {16, "Sixteen"}, {17, "Seventeen"}, {18, "Eighteen"}, {19, "Nineteen"} };
-    unordered_map<int, string> below100 = { {1, "Ten"}, {2, "Twenty"}, {3, "Thirty"}, {4, "Forty"}, {5, "Fifty"}, {6, "Sixty"}, {7, "Seventy"}, {8, "Eighty"}, {9, "Ninety"} };
+    unordered_map<int, string> below100 = {  {2, "Twenty"}, {3, "Thirty"}, {4, "Forty"}, {5, "Fifty"}, {6, "Sixty"}, {7, "Seventy"}, {8, "Eighty"}, {9, "Ninety"} };
     string working(int num){
         if(num < 10) {
             return below10[num];
