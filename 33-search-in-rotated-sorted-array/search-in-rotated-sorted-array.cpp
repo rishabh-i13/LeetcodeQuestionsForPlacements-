2096d5bd84657dc077a96293ba04f81feabc0906
Rishabh Shukla <110485006+rishabh-i13@users.noreply.github.com>
2024-08-09 15:56:07 +0530

    Time: 0 ms (100.00%) | Memory: 14 MB (17.49%) - LeetSync

--- 33-search-in-rotated-sorted-array/search-in-rotated-sorted-array.cpp
+++ 33-search-in-rotated-sorted-array/search-in-rotated-sorted-array.cpp
@@ -1,26 +1,27 @@
 class Solution {
 public:
     int search(vector<int>& nums, int target) {
-        //first search for the index where it is rotated
-        int start=0,end=nums.size()-1;
+        int start=0;
+        int end=nums.size()-1;
+
         while(start<=end){
             int mid=(start+end)/2;
 
             if(nums[mid]==target) return mid;
 
+            //check if target lies in start to and mid
             if(nums[start]<=nums[mid]){
                 if(nums[start]<=target and target<=nums[mid]){
                     end=mid-1;
                 }
                 else start=mid+1;
             }
-            else {
+            else{
                 if(nums[mid]<=target and target<=nums[end]){
                     start=mid+1;
                 }
                 else end=mid-1;
             }
-            
         }
         return -1;
     }
