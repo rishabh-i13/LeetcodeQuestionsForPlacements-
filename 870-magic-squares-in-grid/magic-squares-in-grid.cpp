b0a837fb69fb1097a4698bcdd2d7481eeae35f2b
Rishabh Shukla <110485006+rishabh-i13@users.noreply.github.com>
2024-08-09 16:39:43 +0530

    Time: 8 ms (8.82%) | Memory: 11.8 MB (24.37%) - LeetSync

---
+++ 870-magic-squares-in-grid/magic-squares-in-grid.cpp
@@ -0,0 +1,53 @@
+class Solution {
+public:
+    bool isMagic(vector<vector<int>>& grid,pair<int,int> p,int rows,int cols){
+        int i=p.first;
+        int j=p.second;
+        if(i+2>=rows or j+2>=cols) return false;
+        int x=i,y=j;
+        vector<int> v;
+        for(int m=x;m<=i+2;m++){
+            for(int n=y;n<=j+2;n++){
+                v.push_back(grid[m][n]);
+            }
+        }
+        sort(begin(v),end(v));
+        bool flag=true;
+        for(int k=0;k<v.size();k++){
+            if(v[k]!=k+1){
+                flag=false;
+                break;
+            }
+        }
+        int r1=grid[i][j]+grid[i][j+1]+grid[i][j+2];
+        int r2=grid[i+1][j]+grid[i+1][j+1]+grid[i+1][j+2];
+        int r3=grid[i+2][j]+grid[i+2][j+1]+grid[i+2][j+2];
+
+        int c1=grid[i][j]+grid[i+1][j]+grid[i+2][j];
+        int c2=grid[i][j+1]+grid[i+1][j+1]+grid[i+2][j+1];
+        int c3=grid[i][j+2]+grid[i+1][j+2]+grid[i+2][j+2];
+
+        int d1=grid[i][j]+grid[i+1][j+1]+grid[i+2][j+2];
+        int d2=grid[i][j+2]+grid[i+1][j+1]+grid[i+2][j];
+
+        bool temp=true;
+        if(r1==r2 and r2==r3 and r3==c1 and c1==c2 and c2==c3 and c3==d1 and d1==d2) temp=true;
+        else temp=false;
+
+        if(flag and temp) return true;
+        else return false;
+    }
+
+    int numMagicSquaresInside(vector<vector<int>>& grid) {
+        int rows=grid.size();
+        int cols=grid[0].size();
+        int count=0;
+        for(int i=0;i<rows;i++){
+            for(int j=0;j<cols;j++){
+                if(isMagic(grid,{i,j},rows,cols)==true) count++;
+            }
+        }
+
+        return count;
+    }
+};
