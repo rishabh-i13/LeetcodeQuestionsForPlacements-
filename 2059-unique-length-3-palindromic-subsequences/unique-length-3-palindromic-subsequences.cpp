d986397833017e250320509518f57c5914335471
Rishabh Shukla <110485006+rishabh-i13@users.noreply.github.com>
2025-01-04 22:26:54 +0530

    Time: 160 ms (69.94%) | Memory: 16.1 MB (47.85%) - LeetSync

--- 2059-unique-length-3-palindromic-subsequences/unique-length-3-palindromic-subsequences.cpp
+++ 2059-unique-length-3-palindromic-subsequences/unique-length-3-palindromic-subsequences.cpp
@@ -1,18 +1,32 @@
 class Solution {
 public:
-    int countPalindromicSubsequence(string inputString) {
-    
-    int result = 0;
-    int firstIndex[26] = {[0 ... 25] = INT_MAX};
-    int lastIndex[26] = {};
+    int countPalindromicSubsequence(string s) {
+        unordered_map<char,pair<int,int>> mp;
+        for(char c='a';c<='z';c++){
+            mp[c]={-1,-1};
+        }
+        int n=s.size();
+        for(int i=0;i<n;i++){
+            char c=s[i];
+            if(mp[c].first==-1){mp[c].first=i;};
+        }
+        for(int i=n-1;i>=0;i--){
+            char c=s[i];
+            if(mp[c].second==-1){mp[c].second=i;};
+        }
+        int ans=0;
+        for(auto p:mp){
+            char temp=p.first;
+            int start=p.second.first;
+            int end=p.second.second;
+            unordered_set<char> st;
+            for(int i=start+1;i<end;i++){
+                st.insert(s[i]);
+            }
 
-    for (int i = 0; i < inputString.size(); ++i) {
-        firstIndex[inputString[i] - 'a'] = min(firstIndex[inputString[i] - 'a'], i);
-        lastIndex[inputString[i] - 'a'] = i;
+            ans+=st.size();
+        }
+
+        return ans;
     }
-    for (int i = 0; i < 26; ++i)
-        if (firstIndex[i] < lastIndex[i])
-            result += unordered_set<char>(begin(inputString) + firstIndex[i] + 1, begin(inputString) + lastIndex[i]).size();
-    return result;
-}
 };
