ca5cdea18bf44030205155cb39f722bd6a954122
Rishabh Shukla <110485006+rishabh-i13@users.noreply.github.com>
2025-01-12 22:33:45 +0530

    Time: 31 ms (5.84%) | Memory: 40.5 MB (8.88%) - LeetSync

--- 2221-check-if-a-parentheses-string-can-be-valid/check-if-a-parentheses-string-can-be-valid.cpp
+++ 2221-check-if-a-parentheses-string-can-be-valid/check-if-a-parentheses-string-can-be-valid.cpp
@@ -1,36 +1,29 @@
 class Solution {
 public:
     bool canBeValid(string s, string locked) {
-        int n = s.size();
-        if (n % 2 == 1) return false;
+        int n=s.size();
+        if(n%2==1) return false;
 
-        // Left to Right Check
-        int openCount = 0, wildCount = 0;
-        for (int i = 0; i < n; i++) {
-            if (locked[i] == '1') {
-                if (s[i] == '(') openCount++;
-                else openCount--;
-            } else {
-                wildCount++;
+        stack<pair<char,int>> unlocked;
+        stack<pair<char,int>> opening;
+
+        for(int i=0;i<n;i++){
+            if(locked[i]=='0') unlocked.push({locked[i],i});
+            else if(s[i]=='(') opening.push({s[i],i});
+            else if(s[i]==')'){
+                if(opening.size()>0){
+                    opening.pop();
+                }
+                else if(unlocked.size()>0) unlocked.pop();
+                else return false;
             }
-            // If we have more ')' than possible '('
-            if (openCount + wildCount < 0) return false;
         }
-
-        // Right to Left Check
-        int closeCount = 0;
-        wildCount = 0;
-        for (int i = n - 1; i >= 0; i--) {
-            if (locked[i] == '1') {
-                if (s[i] == ')') closeCount++;
-                else closeCount--;
-            } else {
-                wildCount++;
-            }
-            // If we have more '(' than possible ')'
-            if (closeCount + wildCount < 0) return false;
+        while(unlocked.size() and opening.size() && unlocked.top().second>opening.top().second){
+            opening.pop();
+            unlocked.pop();
         }
-
+        if(opening.size()>0) return false;
         return true;
+
     }
 };
