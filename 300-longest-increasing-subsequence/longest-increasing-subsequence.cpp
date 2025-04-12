8b7bbffb746ad04b570a3eab554b72af98ebfee2
Rishabh Shukla <110485006+rishabh-i13@users.noreply.github.com>
2025-04-12 23:49:23 +0530

    Time: 404 ms (24.27%) | Memory: 41.7 MB (31.00%) - LeetSync

--- 300-longest-increasing-subsequence/longest-increasing-subsequence.cpp
+++ 300-longest-increasing-subsequence/longest-increasing-subsequence.cpp
@@ -1,27 +1,22 @@
 class Solution {
 public:
-    int n;
+//take - not take 
     int dp[2501][2501];
-    int working(vector<int> &nums,int index,int prev){
+    int working(vector<int>& nums,int index,int prev_index,int n){
         if(index>=n) return 0;
-
-        if(prev!=-1 and dp[index][prev]!=-1) return dp[index][prev];
-        //taking ith element
+        //take
+        if(dp[index][prev_index+1]!=-1) return dp[index][prev_index+1];
         int take=0;
-        if(prev==-1 or nums[prev]<nums[index]){
-            take=1+working(nums,index+1,index);
+        if(prev_index==-1 or nums[index]>nums[prev_index]){
+            take=1+working(nums,index+1,index,n);
         }
+        //not take
+        int not_take=working(nums,index+1,prev_index,n);
 
-        //skip ith element
-        int skip=working(nums,index+1,prev);
-
-        if(prev!=-1) return dp[index][prev]=max(take,skip);
-        return max(take,skip);
+        return dp[index][prev_index+1]=max(take,not_take);
     }
     int lengthOfLIS(vector<int>& nums) {
-        n=nums.size();
         memset(dp,-1,sizeof(dp));
-        int prev=-1; //for 1st element
-        return working(nums,0,prev);
+        return working(nums,0,-1,nums.size());  
     }
 };
