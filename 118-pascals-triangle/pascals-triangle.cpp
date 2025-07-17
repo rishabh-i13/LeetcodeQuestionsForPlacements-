cc87224eecb4f182da9e11f27f7ff00d3936ac13
Rishabh Shukla <110485006+rishabh-i13@users.noreply.github.com>
2025-07-18 00:37:11 +0530

    Time: 0 ms (100.00%) | Memory: 9.8 MB (30.99%) - LeetSync

--- 118-pascals-triangle/pascals-triangle.cpp
+++ 118-pascals-triangle/pascals-triangle.cpp
@@ -5,13 +5,13 @@ public:
         ans.push_back({1});
         if(numRows==1) return ans;
         ans.push_back({1,1});
-
-        for(int i=2;i<numRows;i++){
+        if(numRows==2) return ans;
+        int n=numRows-2;
+        for(int i=0;i<n;i++){
             vector<int> temp;
             temp.push_back(1);
-            for(int j=0;j<i-1;j++){
-                int c=ans[i-1][j]+ans[i-1][j+1];
-                temp.push_back(c);
+            for(int j=0;j<ans[i+1].size()-1;j++){
+                temp.push_back(ans[i+1][j]+ans[i+1][j+1]);
             }
             temp.push_back(1);
             ans.push_back(temp);
