034befa52b8530b935c57b77242628e4c227ceba
Rishabh Shukla <110485006+rishabh-i13@users.noreply.github.com>
2025-07-19 03:10:14 +0530

    Time: 8 ms (27.13%) | Memory: 25.6 MB (5.85%) - LeetSync

--- 56-merge-intervals/merge-intervals.cpp
+++ 56-merge-intervals/merge-intervals.cpp
@@ -1,26 +1,25 @@
 class Solution {
 public:
     vector<vector<int>> merge(vector<vector<int>>& intervals) {
-        
-        sort(begin(intervals),end(intervals));
-        int first=intervals[0][0];
-        int second=intervals[0][1];
+        int n=intervals.size();
         vector<vector<int>> ans;
+        sort(intervals.begin(),intervals.end());
 
-        for(int i=1;i<intervals.size();i++){
-            if(second>=intervals[i][0] and second<=intervals[i][1]){
-                second=intervals[i][1];
+        ans.push_back(intervals[0]);
+
+        for(int i=1;i<n;i++){
+            int m=ans.size();
+            vector<int> v=ans[m-1];
+
+            if(v[1]>=intervals[i][0]){
+                ans.pop_back();
+                if(v[1]>=intervals[i][1]) ans.push_back({v[0],v[1]});
+                else ans.push_back({v[0],intervals[i][1]});
             }
-            else if(second>=intervals[i][0] and second>=intervals[i][1]){
-                continue;
-            }
-            else{
-                ans.push_back({first,second});
-                first=intervals[i][0];
-                second=intervals[i][1];
+            else {
+                ans.push_back(intervals[i]);
             }
         }
-        ans.push_back({first,second});
         return ans;
     }
 };
